8a943473663e36867a9fe360b02eb831c4caca1a
popkov <dmitrijp8@gmail.com>
2021-09-08 16:52:01 +0300

    helb2

--- src/main/java/service/StopNameSearcher.java
+++ src/main/java/service/StopNameSearcher.java
@@ -10,16 +10,30 @@ public class StopNameSearcher {
 
     public static List<Stop> findStop(String stopName) {
         List<Stop> resList = new ArrayList<>();
-        for (Stop stop : StopsList.list) {
-            for (String strDb : stop.getStopName().split(" ")) {
-                for (String strNew:stopName.split(" ")) {
-                    if (strDb.toLowerCase().startsWith(strNew.toLowerCase())) {
-                        resList.add(stop);
-                    }
-                }
 
+        for (Stop stop : StopsList.list) {
+            if (stop.getStopName().toLowerCase().startsWith(stopName.toLowerCase())) {
+                resList.add(stop);
             }
         }
+
+        if (resList.isEmpty()){
+            for (Stop stop : StopsList.list) {
+                if (stop.getStopName().toLowerCase().contains(stopName.toLowerCase())) {
+                    resList.add(stop);
+                    continue;
+                }
+                for (String strDb : stop.getStopName().split(" ")) {
+                    for (String strNew:stopName.split(" ")) {
+                        if (strDb.toLowerCase().startsWith(strNew.toLowerCase())) {
+                            resList.add(stop);
+                        }
+                    }
+
+                }
+            }
+        }
+
         for (Stop stop : resList) {
             System.out.println(stop);
         }
